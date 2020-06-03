c470a63ad84e86f250919b46d8fb586aa9ae9a00
mhvarner <62973622+mhvarner@users.noreply.github.com>
2020-06-03 17:13:21 -0400

    Module Work.

---
+++ apps/app.py
@@ -0,0 +1,31 @@
+# Import dependencies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+from scraping import scrape_all
+
+# Setting up Flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Set up App Routes
+# Define the route to the HTML page
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+# Scrape the info
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful!"
+
+# Running Flask
+if __name__ == "__main__":
+    app.run()
+
